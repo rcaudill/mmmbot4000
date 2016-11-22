76c55f42cde4f171eafd3e3c2a41bf95c1de0647
Tif Slama <tif.slama@gmail.com>
2016-11-22 09:37:37 -0800

    added *bop me* that sends a random hanson fact

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -8,20 +8,20 @@
 //   None
 //
 // Commands:
-//  hubot <trigger> - <what the respond trigger does>
+//  bluebot <trigger> - <what the respond trigger does>
 //  <trigger> - <what the hear trigger does>
 //
 // Notes:
 //
 //
 // Author:
-// <github username of the original script author>
+// <tifs>
 //
 
 module.exports = function(robot) {
    // YOUR CODE HERE
    // Example
-   robot.hear(/cheese/, function(msg) {
+   robot.hear(/cheese/i, function(msg) {
      return msg.send("let's get started");
    });
 };
@@ -35,7 +35,43 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 /*
 var squirrels;
 squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+*/
 
+var hansonFacts;
+hansonFacts = [
+"Taylor likes to wear a long braid in the back of his hair.",
+"Together Ike, Tay, and Zac have written over 300 songs.",
+"Ike wears clear braces.",
+"Word has it that Ike names his guitars after girls he likes.",
+"The first song Ike ever wrote was called 'Rain Falling Down' he was in the third grade.",
+"Zac's favorite soda is Dr Pepper.",
+"Back in their home in Tulsa, Ike, Tay, and Zac share a bedroom. Ike and Tay sleep in a bunk bed and Zac has his own bed.",
+"Ike is a Star Wars fan.",
+"The boys love to relax by playing Ping-Pong.",
+"All three boys collect Legos and love to build with them.",
+"Taylor uses Flex shampoo.",
+"Ike use Pantene Pro-V.",
+"Ike likes to wear high-top sneakers or hiking boots.",
+"On days off Ike likes to hang out at the mall.",
+"Zac's favorite school subject is Math.",
+"In the 'MMMBop' video, Taylor is driving the same car Sandra Bullock did in speed 2.",
+"Ike got his first guitar from a pawn shop in Tulsa.",
+"Zac loves action movies.",
+"Zac thinks the best phrase is from Toy Story 'To infinity and beyond!'",
+"One of Hanson's favorite Tulsa restaurants is Rex's Boneless Chicken.",
+"Zac says that 'everday life' inspires him to write songes.",
+"Taylor has a scar on the left side of his face from running into a glass door.",
+"Before Hanson wrote songs about love and friendship, they used to write cute songs about frogs and ants.",
+"Zac has been known to eat whole packets of sugar, straight!"
+];
+
+module.exports = function(robot) {
+  return robot.hear(/bop me/i, function(msg) {
+    return msg.send(msg.random(hansonFacts));
+  });
+};
+
+/*
 module.exports = function(robot) {
   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!"
   return robot.respond(/hi|hello/i, function(msg) {
