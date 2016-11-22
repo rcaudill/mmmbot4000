fc645ef9b8c13043c9057a23d6f0bcde88b03490
Tif Slama <tif.slama@gmail.com>
2016-11-22 09:56:47 -0800

    another try at fixing question mark listener

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -66,7 +66,7 @@ hansonFacts = [
 ];
 
 module.exports = function(robot) {
-  return robot.respond(/"?"/i, function(msg) {
+  return robot.respond(/"*?"/i, function(msg) {
     return msg.send(msg.random(hansonFacts));
   });
 };
